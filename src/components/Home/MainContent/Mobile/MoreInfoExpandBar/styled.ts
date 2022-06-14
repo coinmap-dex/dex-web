310d9eb8a0e93c05cd5c03038494eb9f569ac72b
Hoang Ly <lthtv8@Hoangs-MacBook-Pro.local>
2022-06-14 15:48:14 +0700

    .

--- src/components/Home/MainContent/Mobile/MoreInfoExpandBar/styled.ts
+++ src/components/Home/MainContent/Mobile/MoreInfoExpandBar/styled.ts
@@ -2,8 +2,25 @@ import styled from 'styled-components';
 
 export const MoreInfoExpandContainer = styled.div`
     background: #23304A !important;
+    position:relative;
 `;
 
 export const MoreInfoExpandIconContainer = styled.div`
     margin: 1.648rem auto;
+    position:relative;
+    display:inline-block;
+    &>img:nth-child(1){
+        position:absolute;
+        top:-7px;
+        right:-7px;
+    }
+    &>img:nth-child(2){
+        position:relative;
+    }
+    &>img:nth-child(3){
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform:translate(-50%,-50%);
+    }
 `;
