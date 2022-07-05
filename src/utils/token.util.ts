88953e19980a8e352eebab45ea4aa35d186d41fe
anh-trinh <anh-trinh@outlook.com>
2022-07-05 15:09:37 +0700

    fix(order): modify mapTokenFromOverviewData

--- src/utils/token.util.ts
+++ src/utils/token.util.ts
@@ -2,7 +2,7 @@ import {Token} from '../models/token.model';
 
 export const mapTokenFromOverviewData = (overviewData: any): Token => {
     return {
-        address: overviewData?.contract_address ?? '',
+        address: overviewData?.platforms?.['binance-smart-chain'] ?? '',
         decimals: +overviewData?.decimals ?? 0,
         logo: overviewData?.image?.thumb ?? '',
         name: overviewData?.name ?? '',
